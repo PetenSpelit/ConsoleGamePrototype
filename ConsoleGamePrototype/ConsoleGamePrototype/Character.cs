1c3b208d1f3cbdd9beac59bce0e0d2756b183dc3
C0mmunist1 <36222715+C0mmunist1@users.noreply.github.com>
2018-04-05 17:01:28 +0300

    Add files via upload

---
+++ ConsoleGamePrototype/ConsoleGamePrototype/Character.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleGamePrototype
+{
+    /**
+     *This class will be an abstract to any player, boogieman or other non-playable characters in the game.
+     * 
+     */
+    class Character
+    {
+    }
+}
---
+++ ConsoleGamePrototype/ConsoleGamePrototype/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleGamePrototype
+{
+    class Player
+    {
+        private List<Item> inventory;
+        private String name;
+
+        public Player(String name)
+        {
+            this.name = name;
+            this.inventory = new List<Item>();
+        }
+
+        public void Collect(Item item)
+        {
+            inventory.Add(item);
+        }
+        public List<Item> Inventory
+        {
+            get { return inventory; }
+            set { }
+        }
+    }
+}
