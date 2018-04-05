868fd09ae5e29f5bdbe70022b848ba5a681f55b1
C0mmunist1 <36222715+C0mmunist1@users.noreply.github.com>
2018-04-05 21:37:40 +0300

    Add files via upload

---
+++ Level.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleGamePrototype
+{
+    class Level
+    {
+        private Dictionary<char, int> grid;
+        private GameObject[,] levelGrid;
+        public Level()
+        {
+            this.levelGrid = new GameObject[6,8];
+        }
+        public CheckPlace(GameObject movingObject)
+        {
+            foreach (GameObject temp in levelGrid)
+            {
+                if (temp.Place == movingObject.Place && !temp.WalkOver)
+                {
+                    //implement "don't go"
+                } else if {
+                }
+            }
+        }
+    }
+}
